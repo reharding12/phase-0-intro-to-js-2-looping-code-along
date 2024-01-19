08d1c08d56bacccad9a1883cb5e9075a82cfe36f
Roxanne <reharding12@gmail.com>
2024-01-18 19:41:50 -0700

    passed 3 tests in phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function countDown(number) {
+    let countup = number;
+    while (countup >= 0) {
+      console.log(countup--);
+    }
+  }
+  
+  countDown(10);
+  
+  function writeCards(names, event) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
